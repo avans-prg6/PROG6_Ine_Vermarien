2adc2adc2207c3b3c3e42e07dca59dec7d29bf7f
Ine Vermarien <ieb.vermarien@student.avans.nl>
2025-01-20 15:47:36 +0100

    Beest CRUD
    Beest CRUD
    Admin access
    niet aan accountcontroller en wachtwoord zitten enzo

---
+++ PROG6-2425/PROG6-2425/Repositories/IBeestjeRepository.cs
@@ -0,0 +1,12 @@
+﻿using PROG6_2425.Models;
+
+namespace PROG6_2425.Repositories;
+
+public interface IBeestjeRepository
+{
+    Task<IEnumerable<Beestje>> GetAllAsync();
+    Task<Beestje?> GetByIdAsync(int id);
+    Task CreateAsync(Beestje beestje);
+    Task UpdateAsync(Beestje beestje);
+    Task DeleteAsync(int id);
+}
